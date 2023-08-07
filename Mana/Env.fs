2af7307121b95e8513016a6ca34e5ffa659558ef
Terah <terah.dev@gmail.com>
2023-08-07 15:23:07 +0200

    Add scoped Env

--- Mana/Env.fs
+++ Mana/Env.fs
@@ -1,9 +1,21 @@
 namespace Mana
 
-type Env<'t> = { bindings: Map<string, 't> }
+type Env<'t> = {
+    bindings: Map<string, 't>
+    parent: Env<'t> option
+}
 
 module Env =
-    let empty = { bindings = Map.empty }
+    let empty = {
+        bindings = Map.empty
+        parent = None
+    }
+
+    let scoped env = { empty with parent = env }
 
     let set k v env = { env with bindings = env.bindings |> Map.add k v }
-    let get k env = env.bindings |> Map.tryFind k
+
+    let rec get k env =
+        env.bindings
+        |> Map.tryFind k
+        |> Option.orElse (env.parent |> Option.bind (get k))
